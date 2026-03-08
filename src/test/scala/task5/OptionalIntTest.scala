8e738a475d7e016b2721306fe30109506f7a8649
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2026-03-08 11:07:41 +0100

    minor on test

--- src/test/scala/task5/OptionalIntTest.scala
+++ src/test/scala/task5/OptionalIntTest.scala
@@ -1,6 +1,5 @@
 package task5
 
-
 import org.junit.*
 import org.junit.Assert.*
 import Optionals.*
@@ -18,7 +17,7 @@ class OptionalIntTest:
     val nonEmpty = OptionalInt.Just(0)
     assertEquals(0, OptionalInt.orElse(nonEmpty, 1))
 
-  @Test def orElseShouldReturnValueWhenNonEmpty(): Unit = 
+  @Test def orElseShouldReturnValueWhenNonEmpty(): Unit =
     val empty = OptionalInt.Empty()
     assertEquals(1, OptionalInt.orElse(empty, 1))
 
