26210f20acbcb4845614fe3b63968704880c022d
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-09 11:01:00 +0100

    Es4

---
+++ src/main/scala/it/unibo/pps/u02/Es4.scala
@@ -0,0 +1,15 @@
+package it.unibo.pps.u02
+
+@main
+def es4(): Unit =
+  val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+  val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y == z
+
+  def p3(x: Int)(y: Int)(z: Int) = x <= y && y == z
+
+  def p4(x: Int, y: Int, z: Int) = x <= y && y == z
+
+  println(p1(1)(7)(7))
+  println(p2(1, 7, 7))
+  println(p1(14)(7)(7))
+  println(p2(1, 7, 8))
