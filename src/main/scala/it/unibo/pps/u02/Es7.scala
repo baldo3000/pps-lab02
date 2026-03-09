16c943d36de32993cb3d61437189b14905a0f66b
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-09 11:25:08 +0100

    Es7

---
+++ src/main/scala/it/unibo/pps/u02/Es7.scala
@@ -0,0 +1,23 @@
+package it.unibo.pps.u02
+
+import scala.annotation.tailrec
+
+@main
+def es7(): Unit =
+  def power(base: Double, exponent: Int): Double = exponent match
+    case 0 => 1
+    case _ => base * power(base, exponent - 1)
+
+  println(power(2, 3)) // 8.0
+  println(power(5, 2)) // 25.0
+
+  def powerTail(base: Double, exponent: Int): Double =
+    @tailrec
+    def _power(base: Double, exponent: Int, accumulator: Double): Double = exponent match
+      case 0 => accumulator
+      case _ => _power(base, exponent - 1, accumulator * base)
+
+    _power(base, exponent, 1)
+
+  println(powerTail(2, 3)) // 8.0
+  println(powerTail(5, 2)) // 25.0
