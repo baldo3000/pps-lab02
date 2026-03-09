2ed5411dba570c102f0b182c4a26614bfe0d25df
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-09 10:48:05 +0100

    Es3

---
+++ src/main/scala/it/unibo/pps/u02/Es3.scala
@@ -0,0 +1,25 @@
+package it.unibo.pps.u02
+
+val positiveLambda = (x: Int) => x match
+  case n if n >= 0 => "positive"
+  case _ => "negative"
+
+def positiveMethod(x: Int): String = x match
+  case n if n >= 0 => "positive"
+  case _ => "negative"
+
+val negLambda = (predicate: String => Boolean) => (string: String) => !predicate(string)
+
+def negMethod(predicate: String => Boolean): String => Boolean =
+  string => !predicate(string)
+
+@main
+def mainEs3(): Unit =
+  println(positiveLambda(1))
+  println(positiveMethod(-1))
+
+  val empty: String => Boolean = _ == ""
+  val notEmpty = negLambda(empty)
+  println(notEmpty("foo"))
+  println(notEmpty(""))
+  println(notEmpty("foo") && !notEmpty(""))
